796a12c124b4bc2664dab2c79245b39638993a11
Robert <robertwinstonanderson@gmail.com>
2019-04-02 16:27:42 -0600

    testing some more

--- SocketManager.js
+++ SocketManager.js
@@ -8,9 +8,10 @@ module.exports = function(socket) {
     socket.on("userConnected", () => {
         let questions = [];
         axios.get("https://gameday-testing.herokuapp.com/api/getquestions").then((qstns) => {
-            questions = qstns.data;
+            // questions = qstns.data;
+            questions = "these are questions";
             
-            io.emit("gotquestions", "Got yer questions right here!!!");
+            io.emit("gotquestions", questions);
         }).catch(err => console.log(err));
     })
 };
